6ec1cdf3c910b7ddde52781101fd9eefc68a7fac
Pascal Rusch <git@paru.ch>
2013-11-09 19:16:44 +0100

    exception class for the tool added

---
+++ tools-common/src/main/java/ch/paru/scrumTools/common/capacity/ToolException.java
@@ -0,0 +1,8 @@
+package ch.paru.scrumTools.common.capacity;
+
+public class ToolException extends RuntimeException {
+
+	public ToolException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+}
