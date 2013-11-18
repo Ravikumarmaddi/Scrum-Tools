3d00a5e61fc10933d184abf3b8e7adae01dae469
Kerli81 <git@paru.ch>
2013-11-18 21:05:16 +0100

    ToolException is the base exception for everything

--- scrumTools/src/main/java/ch/paru/scrumTools/exchangeServer/utils/exceptions/ServerException.java
+++ scrumTools/src/main/java/ch/paru/scrumTools/exchangeServer/utils/exceptions/ServerException.java
@@ -1,7 +1,8 @@
 package ch.paru.scrumTools.exchangeServer.utils.exceptions;
 
+import ch.paru.scrumTools.common.exception.ToolException;
 
-public class ServerException extends RuntimeException {
+public class ServerException extends ToolException {
 
 	public ServerException(final String message, final Throwable cause) {
 		super(message, cause);
