6896245ad802b4dd6d9884b41d33423dbe549a78
Pascal Rusch <git@paru.ch>
2013-12-15 14:00:12 +0100

    calculate & show custom detail table

--- scrumTools-custom/src/main/java/ch/paru/scrumTools/custom/capacity/sprint/CustomSprintOutputGenerator.java
+++ scrumTools-custom/src/main/java/ch/paru/scrumTools/custom/capacity/sprint/CustomSprintOutputGenerator.java
@@ -1,8 +1,13 @@
 package ch.paru.scrumTools.custom.capacity.sprint;
 
+import java.util.List;
+
+import ch.paru.scrumTools.capacity.shared.data.Numbers;
+import ch.paru.scrumTools.capacity.shared.data.Team;
 import ch.paru.scrumTools.capacity.sprint.data.SprintData;
 import ch.paru.scrumTools.capacity.sprint.renderer.generator.SprintOutputGenerator;
 import ch.paru.scrumTools.capacity.sprint.renderer.generator.XlsSprintOutputGenerator;
+import ch.paru.scrumTools.common.formatting.RoundingUtil;
 import ch.paru.scrumTools.common.reflection.customs.Custom;
 import ch.paru.scrumTools.exchangeServer.utils.ServerDayUtil;
 
@@ -17,6 +22,50 @@ public class CustomSprintOutputGenerator extends XlsSprintOutputGenerator {
 		sb.append(" - ");
 		sb.append(ServerDayUtil.getDisplayText(data.getEndDay()));
 		sb.append(NEWLINE);
+		sb.append(NEWLINE);
+		return sb.toString();
+	}
+
+	@Override
+	public String getPostTableData(SprintData data) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(NEWLINE);
+		sb.append(NEWLINE);
+
+		sb.append(getDetailHeader());
+
+		List<Team> allTeams = data.getAllTeams();
+		for (Team team : allTeams) {
+			sb.append(getTeamDetailLine(team));
+		}
+
+		return sb.toString();
+	}
+
+	private String getTeamDetailLine(Team team) {
+		Numbers numbers = team.getNumbers();
+
+		StringBuffer sb = new StringBuffer();
+		sb.append(team.getName());
+		sb.append(TAB);
+		sb.append(RoundingUtil.roundFullUp(numbers.getAvailability()));
+		sb.append(TAB);
+		sb.append(RoundingUtil.roundFullUp(numbers.getRawCapacity()));
+		sb.append(TAB);
+		sb.append(RoundingUtil.roundFullUp(numbers.getFinalCapacity()));
+		sb.append(NEWLINE);
+		return sb.toString();
+	}
+
+	private String getDetailHeader() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Team name");
+		sb.append(TAB);
+		sb.append("Team Availability");
+		sb.append(TAB);
+		sb.append("Team Raw Capacity");
+		sb.append(TAB);
+		sb.append("Team Final Capacity");
 		sb.append(NEWLINE);
 		return sb.toString();
 	}
