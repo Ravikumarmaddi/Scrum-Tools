ddd622458cc93821a9be1179e04f0256ef01b110
Kerli81 <git@paru.ch>
2014-01-09 23:50:39 +0100

    adjust deployment script

--- scrumTools/release-version.sh
+++ scrumTools/release-version.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-VERSION_RELEASE=1.1.1
+VERSION_RELEASE=1.1.2-SNAPSHOT
 VERSION_DEVELOPMENT=1.1.2-SNAPSHOT
 REPO=/home/neo/ws-git/mvn-repo
 
-mvn versions:set -DnewVersion=$VERSION_RELEASE
+VERSION_CMD=org.codehaus.mojo:versions-maven-plugin:2.1:set
+
+cd deploy
+mvn $VERSION_CMD -DnewVersion=$VERSION_RELEASE
+cd ..
 
 mvn package
 mvn source:jar
 
-mvn install:install-file -Dfile=target/scrumTools-$VERSION_RELEASE.jar -Dsources=target/scrumTools-$VERSION_RELEASE-sources.jar -DpomFile=pom.xml -DlocalRepositoryPath=$REPO
+mvn install:install-file -Dfile=target/scrumTools-$VERSION_RELEASE.jar -Dsources=target/scrumTools-$VERSION_RELEASE-sources.jar -DpomFile=deploy/pom.xml -DlocalRepositoryPath=$REPO -DcreateChecksum=true
 
-mvn versions:set -DnewVersion=$VERSION_DEVELOPMENT
+cd deploy
+mvn $VERSION_CMD -DnewVersion=VERSION
+cd ..
+
+mvn $VERSION_CMD -DnewVersion=$VERSION_DEVELOPMENT
+
+rm **/pom.xml.versionsBackup
+
