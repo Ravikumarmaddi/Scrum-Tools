f1d558974f6164ebe31c3b877eb6c3c31f8fc505
Kerli81 <git@paru.ch>
2014-02-04 14:02:59 +0100

    commit and tag changes in the release-version script

--- scrumTools/release-version.sh
+++ scrumTools/release-version.sh
@@ -16,6 +16,16 @@ mvn source:jar
 
 mvn install:install-file -Dfile=target/scrumTools-$VERSION_RELEASE.jar -Dsources=target/scrumTools-$VERSION_RELEASE-sources.jar -DpomFile=deploy/pom.xml -DlocalRepositoryPath=$REPO -DcreateChecksum=true
 
+rm deploy/pom.xml.versionsBackup
+rm pom.xml.versionsBackup
+
+git add .
+git commit -m "changes for version $VERSION_RELEASE"
+echo "files to git added"
+
+git tag -a v$VERSION_RELEASE -m "v$VERSION_RELEASE"
+echo "git tag $VERSION_RELEASE created"
+
 cd deploy
 mvn $VERSION_CMD -DnewVersion=VERSION
 cd ..
@@ -23,4 +33,7 @@ mvn $VERSION_CMD -DnewVersion=$VERSION_DEVELOPMENT
 
 rm deploy/pom.xml.versionsBackup
 rm pom.xml.versionsBackup
+
+git add .
+git commit -m "changes for development version $VERSION_DEVELOPMENT"
 
